83ad926374571d481267a0579fca75cc02324855
Taylor Pubins <tpubz@icloud.com>
2023-11-28 10:18:56 -0700

    'Add VCR cassette and re-record interval'

--- spec/features/user_can_search_by_state_spec.rb
+++ spec/features/user_can_search_by_state_spec.rb
@@ -1,30 +1,29 @@
 require "rails_helper"
 
 feature "user can search for house members" do
-  scenario "user submits valid state name" do
+  scenario "user submits valid state name", :vcr do
     # As a user
     # When I visit "/"
-    VCR.use_cassette("propublica_members_of_the_senate_for_CO") do
-      visit "/"
+    visit "/"
 
-      select "Colorado", from: :state
-      # And I select "Colorado" from the dropdown
-      click_on "Locate Members of the House"
-      # And I click on "Locate Members from the House"
-      expect(current_path).to eq(search_path)
-      # Then my path should be "/search" with "state=CO" in the parameters
-      expect(page).to have_content("8 Results")
-      # And I should see a message "8 Results"
-      expect(page).to have_css(".member", count: 8)
-      # And I should see a list of 8 the members of the house for Colorado
+    select "Colorado", from: :state
+    # And I select "Colorado" from the dropdown
+    click_on "Locate Members of the House"
+    # And I click on "Locate Members from the House"
+    expect(current_path).to eq(search_path)
+    # Then my path should be "/search" with "state=CO" in the parameters
+    expect(page).to have_content("8 Results")
+    # And I should see a message "8 Results"
+    expect(page).to have_css(".member", count: 8)
+    # And I should see a list of 8 the members of the house for Colorado
 
-      within(first(".member")) do
-        expect(page).to have_css(".name")
-        expect(page).to have_css(".role")
-        expect(page).to have_css(".party")
-        expect(page).to have_css(".district")
-      end
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".role")
+      expect(page).to have_css(".party")
+      expect(page).to have_css(".district")
     end
+
     # And I should see a name, role, party, and district for each member
   end
 end
